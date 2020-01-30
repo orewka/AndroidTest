8fe295876629b429d31240ac8889620212819a0c
sawok <orewka@inbox.ru>
2020-01-30 19:03:31 +0300

    2

--- app/src/main/java/com/example/myapplication/ListDevices.java
+++ app/src/main/java/com/example/myapplication/ListDevices.java
@@ -9,19 +9,24 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListDevices extends AppCompatActivity {
     LocalService mService;
     boolean mBound = false;
+    public List<String> listDevices = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_devices);
     }
 
     @Override
@@ -29,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Intent intent = new Intent(this, LocalService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        listDevices = mService.getListDevices();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ListView listView = findViewById(R.id.listDevices);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listDevices);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view;
+                Toast.makeText(getBaseContext(), textView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -38,14 +60,6 @@ public class MainActivity extends AppCompatActivity {
             unbindService(mConnection);
             mBound = false;
         }
-    }
-
-    public void onButtonClickLed(View v) {
-        if (mBound) {
-            mService.sendType('L');
-        }
-        Intent intent = new Intent(MainActivity.this, ListDevices.class);
-        startActivity(intent);
     }
 
     private ServiceConnection mConnection = new ServiceConnection() {
--- app/src/main/java/com/example/myapplication/LocalService.java
+++ app/src/main/java/com/example/myapplication/LocalService.java
@@ -2,26 +2,36 @@ package com.example.myapplication;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
-import androidx.annotation.Nullable;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocalService extends Service {
+    private final IBinder mBinder = new LocalBinder();
     private Socket socket;
     private DataOutputStream dataOutputStream;
+    private DataInputStream dataInputStream;
 
     @Override
     public void onCreate() {
-        Thread getSocket = new Thread(new Runnable() {
+        super.onCreate();
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
                     socket = new Socket("10.0.2.2", 56);
                     dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                    dataInputStream = new DataInputStream(socket.getInputStream());
                     dataOutputStream.writeChar('a');
                     dataOutputStream.flush();
                 } catch (IOException e) {
@@ -29,29 +39,41 @@ public class LocalService extends Service {
                 }
             }
         });
-        getSocket.start();
-        super.onCreate();
+        thread.start();
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        try {
-            dataOutputStream.writeChar('b');
-            dataOutputStream.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public class LocalBinder extends Binder {
+        LocalService getService() {
+            return LocalService.this;
         }
-        return super.onStartCommand(intent, flags, startId);
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
+    }
+
+    public void sendType(final Character character) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    dataOutputStream.writeChar(character);
+                    dataOutputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+    public List<String> getListDevices() {
+        StringReader stringReader = new StringReader();
+        Type type = new TypeToken<List<String>>() {}.getType();
+        List<String> listDevices = new ArrayList<>();
+        String listDevicesJson = stringReader.readCommand(dataInputStream);
+        listDevices = new Gson().fromJson(listDevicesJson, type);
+        return listDevices;
     }
 }
